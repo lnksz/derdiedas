1320c6b520d88ea20ce12c50e54f7588dfcb153c
Kristof Havasi <havasi@efr.de>
2024-02-11 06:43:42 +0100

    Create basic UI placeholders

--- derdiedas/src/app/app.component.ts
+++ derdiedas/src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { A11yModule } from '@angular/cdk/a11y'
+
+export interface Word {
+  word: string;
+  article: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>Was ist der Artikel für </h1>
-  `,
+  imports: [CommonModule, MatInputModule, A11yModule],
+  templateUrl: 'app.component.html',
   styleUrl: 'app.scss',
 })
 export class AppComponent {
   title = 'Der Die Das';
+  res: Word = { word: '', article: '' };
+  word = '';
+  @ViewChild('wordInput') wordInput!: MatInputModule;
+
+  onKey(value: string) {
+    // This will suggest auto complete
+    this.word = value;
+  }
+  onEnter(value: string) {
+    // This will look up the word
+    this.word = value;
+    alert(this.word);
+  }
 }
