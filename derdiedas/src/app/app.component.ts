f692d4c1be88d3bd72608acbe6ce687579b8b4c6
Kristof Havasi <havasi@efr.de>
2024-02-11 05:27:11 +0100

    Angular cleanup

--- derdiedas/src/app/app.component.ts
+++ derdiedas/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   template: `
     <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
   `,
   styles: [],
 })
