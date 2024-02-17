b3a9d971533bb7a347aa2cbb27b5b341957145d3
Kristóf Havasi <lnksz@users.noreply.github.com>
2024-02-17 14:54:05 +0000

    Manual input debounce

--- derdiedas/src/app/app.component.ts
+++ derdiedas/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
+import { MatInput, MatInputModule } from '@angular/material/input';
 import { A11yModule } from '@angular/cdk/a11y'
 import { LookupService, Word } from './lookup.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,25 @@ export class AppComponent {
   title = 'Der Die Das';
   results: Word[] = [];
   word = '';
+  limitSearchLengthTrigger = 2;
+  lastTakenKeyAt = Date.now();
+  debounceTime = 200; //ms
 
   constructor(private lookupService: LookupService) { }
 
   onKey(value: string) {
+    const nowPressedAt = Date.now();
+    this.results = [];
+    if (nowPressedAt - this.lastTakenKeyAt < this.debounceTime)
+      return;
+    else
+      this.lastTakenKeyAt = nowPressedAt;
+
     this.word = value.trim();
+    if (this.word.length < this.limitSearchLengthTrigger) {
+      this.results = [];
+      return;
+    }
     // this.res = this.lookupService.get(this.word);
     let results = this.lookupService.getPrefixed(this.word);
     if (results.length === 0) {
